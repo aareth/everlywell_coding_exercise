3fd553e5673f501c1b00915fe3b24ed817f2908e
jbogedain <jerry.bogedain@gmail.com>
2018-09-23 14:53:09 -0500

    fix firned.js to always load when show page is rendered

--- app/assets/javascripts/friend.js
+++ app/assets/javascripts/friend.js
@@ -1,17 +1,17 @@
 
-$( document ).ready(function() {
+$('.experts.show').ready(function() {
     $('#add_friend_button').click(function() {
       var data = {};
       data['friend'] = $('#friend_to_add').val();
 
       $.post('/experts/' + $('#expert_id').val() + '/add_friend', data);
-    })
+    });
 
     $('#find_friend_button').click(function() {
       var subject = encodeURIComponent(($('#friend_subject_to_add').val()).trim());
       $.get('/experts/' + $('#expert_id').val() + '/find_friend?subject=' + subject, writeOutPath);
-    })
-});
+    });
+})
 
 function writeOutPath(path) {
   $('#path-body').html('');
